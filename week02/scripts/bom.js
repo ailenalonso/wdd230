924ff6b6e46ba3c584403f642ea398b35d76e9cc
ailenalonso <ailu.alonso@gmail.com>
2023-09-21 13:08:49 -0300

    dom activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('#submitbutton');
+const list = document.querySelector('.list'); 
+console.log(input);
+
+button.addEventListener('click', addChapter);
+
+function addChapter() {
+    if (input.value != "") {
+        /* Get the content of input */
+        inputContent = input.value;
+        /* Create li and button element */
+        li = document.createElement('li'); 
+        deleteButton = document.createElement('button');
+        /* Add content to the elements*/
+        li.textContent = inputContent;
+        deleteButton.textContent = "❌";
+        /* Append the elements */
+        li.appendChild(deleteButton)
+        list.appendChild(li);
+        /* Delete button event listener, empty input box and focus */
+        deleteButton.addEventListener('click', test1 => {
+            document.querySelector('.list li').remove();
+            input.focus();
+        });
+        input.value = '';
+        input.focus();
+    }   
+}
