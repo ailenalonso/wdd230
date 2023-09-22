a8db4c3d6b6f356f1a1b21955cceccaada76b049
ailenalonso <ailu.alonso@gmail.com>
2023-09-22 00:01:15 -0300

    dark mode

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -24,3 +24,19 @@ function toggleMenu(){
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+//Dark Mode
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
