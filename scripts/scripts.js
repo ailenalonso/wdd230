24e497e5db8589151e7bf3ddc6a7292d19c17ecc
ailenalonso <ailu.alonso@gmail.com>
2023-10-06 12:42:05 -0300

    adding js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -77,3 +77,14 @@ function checkSame() {
     p2.style.backgroundColor = "#ffffff";
   }
 }
+
+//Range display
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+range.addEventListener("change", displayRatingValue);
+range.addEventListener("input", displayRatingValue);
+
+function displayRatingValue() {
+  rangevalue.innerHTML = range.value;
+}
