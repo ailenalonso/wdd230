a62a625a0167fb0c6fdf3d6e91a66a387ee4433f
ailenalonso <ailu.alonso@gmail.com>
2023-10-19 17:48:01 -0300

    updating

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,15 +1,14 @@
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption");
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=f45d3310006c3e747103fecf995bbd92";
+const description = document.querySelector("#weather-description");
+const place = document.querySelector("#weather-location");
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=-34.52&lon=-58.53&units=imperial&appid=f45d3310006c3e747103fecf995bbd92";
 
 async function apiFetch() {
   try {
     let response = await fetch(url);
     if (response.ok) {
       let data = await response.json();
-
-      console.log(data);
 
       displayResults(data);
     } else {
@@ -21,12 +20,13 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
   const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
   let desc = data.weather[0].description;
   weatherIcon.setAttribute("src", iconsrc);
   weatherIcon.setAttribute("alt", desc);
-  captionDesc.textContent = `${desc}`;
+  description.textContent = `${desc}`;
+  place.textContent = `${data.name}, ${data.sys.country}`;
 }
 
 apiFetch();
