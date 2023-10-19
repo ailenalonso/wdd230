213d3922c370641e96d8fbb1332b6eccba90e2f8
ailenalonso <ailu.alonso@gmail.com>
2023-10-19 11:32:25 -0300

    updating

---
+++ scripts/links.js
@@ -0,0 +1,39 @@
+const baseURL = "https://ailenalonso.github.io/wdd230/";
+const linksURL = "https://ailenalonso.github.io/wdd230/data/links.json";
+const weeksList = document.querySelector(".weeks-list");
+
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+
+  displayLinks(data.weeks);
+}
+
+function displayLinks(weeks) {
+  weeks.forEach((week) => {
+    let newWeek = document.createElement("li");
+    newWeek.textContent = `${week.week}`;
+
+    week.links.forEach((link) => {
+      let spacer = document.createElement("span");
+      let url = document.createElement("a");
+
+      if (link == week.links[0]) {
+        spacer.textContent = `: `;
+      } else {
+        spacer.textContent = ` | `;
+      }
+      url.setAttribute("href", `${link.url}`);
+      url.textContent = `${link.title}`;
+
+      if (url.textContent !== "") {
+        newWeek.appendChild(spacer);
+      }
+      newWeek.appendChild(url);
+    });
+
+    weeksList.appendChild(newWeek);
+  });
+}
+
+getLinks();
